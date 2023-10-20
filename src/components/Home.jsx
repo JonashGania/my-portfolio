2b60200730ec4a040fdd3b47fc4514227821d473
Jonash Gania <jonashtiglao@gmail.com>
2023-10-20 16:09:41 +0800

    components folder

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Home(){
+    return (
+        <div className=" max-w-960 mx-auto">
+            <h1 className=" text-center">Home</h1>
+        </div>
+    )
+}
+ 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Link, NavLink } from "react-router-dom"
+
+export default function Navbar(){
+    return(
+        <header className="w-full bg-midnight flex justify-center">
+            <nav className="w-960 mx-4 flex items-center justify-between gap-4 h-24">
+                <Link to="/" className="text-white text-xl px-2 py-1 border-2 border-white rounded-md">Nash.</Link>
+                <ul className="flex items-center gap-14">
+                    <li className="text-gray-400 text-lg">
+                        <NavLink to="/about" className="py-2 px-3 rounded-md">About</NavLink>
+                    </li>
+                    <li className="text-gray-400 text-lg">
+                        <NavLink to="/projects" className='py-2 px-3 rounded-md'>Projects</NavLink>
+                    </li>
+                    <li className="text-gray-400 text-lg py-1 px-3 rounded-md">
+                        <NavLink to="/experience" className='py-2 px-3 rounded-md'>Experience</NavLink>
+                    </li>
+                </ul>
+                <button className="bg-white outline-none px-3 py-2 font-medium rounded-md">Get in touch</button>
+            </nav>
+        </header>
+    )
+}
