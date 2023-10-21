6c767f7a154a80125b9fc29639a09b5297a39229
Jonash Gania <jonashtiglao@gmail.com>
2023-10-21 16:30:07 +0800

    added projects

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,6 +1,13 @@
 const data = {
     name: "Jonash Gaña",
     profileUrl: "src/assets/profile.png",
+    projects: [
+        {
+            title: 'TaskFlow',
+            description: 'TaskFlow is a todo-list app where you can set different tasks for today and this week.',
+            tools: ['HTML', 'CSS', 'JavaScript'],
+        }
+    ]
 }
 
 export default data
