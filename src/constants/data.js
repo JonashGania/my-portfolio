20f8353a220e468751a04b250d3b7f85f0837dd1
Jonash Gania <jonashtiglao@gmail.com>
2023-10-22 22:15:02 +0800

    added social links

--- src/constants/data.js
+++ src/constants/data.js
@@ -6,8 +6,16 @@ const data = {
             title: 'TaskFlow',
             description: 'TaskFlow is a todo-list app where you can set different tasks for today and this week.',
             tools: ['HTML', 'CSS', 'JavaScript'],
+            link: '',
+            imgUrl: 'src/assets/taskflow.png'
         }
-    ]
+    ],
+    socialLinks: {
+        github: 'https://github.com/JonashGania',
+        linkedIn: '',
+        instagram: '',
+        facebook: 'https://www.facebook.com/nashhh.30/'
+    }
 }
 
 export default data
