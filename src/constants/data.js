163f81792f8fce71322e7d3df96101b510378ed2
Jonash Gania <jonashtiglao@gmail.com>
2023-10-24 22:53:46 +0800

    added certificates

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,15 +1,33 @@
 const data = {
     name: "Jonash Gaña",
-    profileUrl: "src/assets/profile.png",
+    profileUrl: "/profile.png",
     projects: [
         {
             title: 'TaskFlow',
             description: 'TaskFlow is a todo-list app where you can set different tasks for today and this week.',
             tools: ['HTML', 'CSS', 'JavaScript'],
             link: '',
-            imgUrl: 'src/assets/taskflow.png'
+            imgUrl: '/taskflow.png'
         },{
             title: 'Weather-app',
+        }
+    ],
+    certificates: [
+        {
+            name: 'IT Essentials',
+            company: 'Cisco',
+            link: 'src/certificates/ITE CPHWFUND 2nd-certificate.pdf',
+            id: 0
+        },{
+            name: 'Introduction to Networks',
+            company: 'Cisco',
+            link: 'src/certificates/CCNAv7 ITN INCOM-certificate.pdf',
+            id: 1
+        }, {
+            name: 'Enterprise Networking, Security, and Automation',
+            company: 'Cisco',
+            link: 'src/certificates/CCNAv7 ENSA INCO-certificate.pdf',
+            id: 2
         }
     ],
     socialLinks: {
