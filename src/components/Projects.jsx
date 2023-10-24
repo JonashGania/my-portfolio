0680d118e543ca92dbb71e888fb953c97c0d822b
Jonash Gania <jonashtiglao@gmail.com>
2023-10-24 22:53:20 +0800

    added projects heading

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,10 @@
 
 export default function Projects(){
     return (
-        <section>
-            <h1>Projects</h1>
+        <section className="w-full min-h-screen bg-midnight pt-24">
+            <div className="max-w-960 mx-auto">
+                <h1 className="text-white pt-32 font-bold text-[40px]">Projects</h1>
+            </div>
         </section>
     )
 }
