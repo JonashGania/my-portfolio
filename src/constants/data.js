cbc4a9127c1a8e17473538de30eadd766a120859
Jonash Gania <jonashtiglao@gmail.com>
2023-10-25 15:03:15 +0800

    Added projects data

--- src/constants/data.js
+++ src/constants/data.js
@@ -4,12 +4,22 @@ const data = {
     projects: [
         {
             title: 'TaskFlow',
-            description: 'TaskFlow is a todo-list app where you can set different tasks for today and this week.',
+            description: 'TaskFlow is a todo-list app which is designed to help users organize tasks, activities, and responsibilities',
             tools: ['HTML', 'CSS', 'JavaScript'],
             link: '',
             imgUrl: '/taskflow.png'
-        },{
+        }, {
             title: 'Weather-app',
+            description: 'Weather-app uses weather APIs to deliver you accurate weather conditions, temperature, humidity, and wind speed for a specific locations.',
+            tools: ['HTML', 'CSS', 'JavaScript'],
+            link: '',
+            imgUrl: '/weather-app.png'
+        }, {
+            title: 'Tic-Tac-Toe',
+            description: 'Tic-Tac-Toe is an interactive and classic two-player board game.',
+            tools: ['HTML', 'CSS', 'JavaScript'],
+            link: '',
+            imgUrl: '/ticTacToe.png'
         }
     ],
     certificates: [
