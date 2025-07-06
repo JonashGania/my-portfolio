23fdbfb38f3080236f5e86c0a96bc2dac94a9689
JonashGana <jonashtiglao@gmail.com>
2025-07-06 12:40:31 +0800

    changed bg color

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -2,7 +2,7 @@ import data from "../constants";
 
 const Certificates = () => {
   return (
-    <div className="w-full sm:w-[300px] md:w-[390px] px-4 py-2 border border-gray-200 dark:bg-[#111] dark:border-[#333] rounded-lg">
+    <div className="w-full sm:w-[300px] md:w-[390px] px-4 py-2 border border-gray-200 dark:bg-neutral-900/70 dark:border-[#333] rounded-lg">
       <h2 className="text-lg font-semibold">Certificates</h2>
       <div className="certificate-grid gap-1">
         {data.certificates.map((cert) => (
