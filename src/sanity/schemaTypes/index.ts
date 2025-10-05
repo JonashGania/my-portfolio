7df522b39893af1b6b94dac676873cbe80556396
JonashGana <jonashtiglao@gmail.com>
2025-10-05 19:45:15 +0800

    experience and certificate schema

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,3 +1,5 @@
 import { project } from "./project";
+import { experience } from "./experience";
+import { certificate } from "./certificate";
 
-export const schema = [project];
+export const schema = [project, experience, certificate];
