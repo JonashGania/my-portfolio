eb3c65d9e90894f662c51d042e123ff2dc47651c
JonashGana <jonashtiglao@gmail.com>
2025-10-17 15:11:20 +0800

    removed

--- src/sanity/schemaTypes/index.ts
+++
@@ -1,5 +0,0 @@
-import { project } from "./project";
-import { experience } from "./experience";
-import { certificate } from "./certificate";
-
-export const schema = [project, experience, certificate];
