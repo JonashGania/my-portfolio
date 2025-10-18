e2601d09fde0eb4d277ac823ef15bf1da658ff5f
JonashGana <jonashtiglao@gmail.com>
2025-10-18 19:42:19 +0800

    Profile schema

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import { project } from "./project";
 import { experience } from "./experience";
 import { certificate } from "./certificate";
+import { profile } from "./profile";
 
-export const schema = [project, experience, certificate];
+export const schema = [project, experience, certificate, profile];
