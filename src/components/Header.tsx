05b36f47423dbcf7946b3156bdbf9442cd3156e1
JonashGana <jonashtiglao@gmail.com>
2025-10-18 19:40:41 +0800

    fetch profile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import { MapPin, Mail } from "lucide-react";
+import { getProfile } from "../lib/queries";
 import Image from "next/image";
 import data from "../constants";
 import ThemeSwitch from "./ThemeSwitch";
+import DownloadResume from "./DownloadResume";
 
-const Header = () => {
+const Header = async () => {
+  const profile = await getProfile();
+  console.log(profile);
   return (
-    <section className="flex justify-between items-start pt-14">
+    <section className="pt-14">
       <div className="flex gap-4 sm:gap-8 items-center animate-in">
         <Image
           src={"/images/avatar.jpg"}
@@ -13,39 +17,45 @@ const Header = () => {
           height={160}
           width={160}
           priority
-          className="rounded-md md:rounded-xl w-32 h-32 sm:w-40 sm:h-40 object-cover"
+          className="rounded-md md:rounded-xl w-40 sm:h-40 object-cover"
         />
-        <div className="flex flex-col">
-          <h1 className="text-zinc-700 dark:text-white text-xl duration-100 transition-color ease-in-out sm:text-2xl font-bold">
-            Jonash Gaña
-          </h1>
+        <div className="flex flex-col flex-1 w-full">
+          <div className="flex items-center justify-between">
+            <h1 className="text-zinc-700 dark:text-white text-xl duration-100 transition-color ease-in-out sm:text-2xl font-bold">
+              {profile.name}
+            </h1>
+            <ThemeSwitch />
+          </div>
+
           <div className="flex items-center gap-1">
             <MapPin
               size={15}
               className="text-zinc-700 dark:text-gray-200 duration-100 transition-color ease-in-out"
             />
             <span className="text-zinc-700 dark:text-gray-200 text-xs sm:text-sm duration-100 transition-color ease-in-out">
-              Pampanga, Philippines
+              {profile.location}
             </span>
           </div>
-          <h2 className="text-base text-zinc-700 dark:text-gray-200 font-medium pt-2 duration-100 transition-color ease-in-out">
-            Full Stack Developer
+          <h2 className="text-sm min-[500px]:text-base text-zinc-700 dark:text-gray-200 font-medium pt-2 duration-100 transition-color ease-in-out">
+            {profile.position}
           </h2>
-          <a
-            href={`mailto:${data.socialLinks.email}`}
-            target="_blank"
-            rel="noreferrer"
-            className="bg-black dark:bg-white dark:hover:bg-white/90 w-[150px] dark:text-black text-white text-sm outline-none py-2 mt-6 rounded-md flex items-center gap-2 justify-center duration-100 transition-all ease-in-out"
-          >
-            <Mail
-              size={18}
-              className="text-white dark:text-black duration-100 transition-color ease-in-out"
-            />
-            Contact me
-          </a>
+          <div className="flex sm:items-center flex-col min-[500px]:flex-row gap-1 min-[500px]:gap-4 mt-2 min-[500px]:mt-6">
+            <a
+              href={`mailto:${data.socialLinks.email}`}
+              target="_blank"
+              rel="noreferrer"
+              className="bg-black dark:bg-white dark:hover:bg-white/90 border px-5 dark:text-black text-white text-xs outline-none py-2 rounded-lg flex items-center gap-2 hover:-translate-y-[2px] justify-center duration-100 transition-all ease-in-out"
+            >
+              <Mail
+                size={15}
+                className="text-white dark:text-black duration-100 transition-color ease-in-out"
+              />
+              Contact me
+            </a>
+            <DownloadResume resumeUrl={profile.resumeUrl} />
+          </div>
         </div>
       </div>
-      <ThemeSwitch />
     </section>
   );
 };
